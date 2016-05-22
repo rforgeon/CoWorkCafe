b7a96be2dd91da00fd67e9ccd08eb47714071479
rforgeon <rforgeon@gmail.com>
2016-05-21 17:18:16 -0700

    spinner2

--- app/assets/javascripts/spinner.js
+++ app/assets/javascripts/spinner.js
@@ -1,11 +1,1 @@
-$( document ).ready(function() {
-  //display spinner between pages
-  $(document).on("page:fetch", function(){
-    $(".sk-folding-cube").show();
-  });
 
-  $(document).on("page:receive", function(){
-    $(".sk-folding-cube").hide();
-  });
-
-});
